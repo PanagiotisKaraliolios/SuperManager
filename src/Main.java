a0d3f8ed9fe90240ca816b24ca39d310451468b3
TheofilosKiapidis <theofiloskiapidis@protonmail.com>
2020-05-01 15:56:45 +0300

    Δημιουργία κλάσης Product / Μικρή μορφοποίηση της main;

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
 
-public class Main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class Main
+{
+	public static void main(String[] args)
+	{
 		
 	}
 
---
+++ src/Product.java
@@ -0,0 +1,73 @@
+public class Product
+{
+	private String name;
+	private double price;
+	private String stockType;
+	private int productsID;
+	private int suppliersID;
+	
+	//Constructor
+	public Product(String name, double price, String stockType, int productsID, int suppliersID)
+	{
+		this.name = name;
+		this.price = price;
+		this.stockType = stockType;
+		this.productsID = productsID;
+		this.suppliersID = suppliersID;
+	}
+	
+	//get methods
+	public String getName()
+	{
+		return this.name;
+	}
+	
+	public double getPrice()
+	{
+		return this.price;
+	}
+	
+	public String getStockType()
+	{
+		return this.stockType;
+	}
+	
+	public int getProductsID()
+	{
+		return this.productsID;
+	}
+	
+	public int getSuppliersID()
+	{
+		return this.suppliersID;
+	}
+	
+	
+	//set methods
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	public void setPride(double price)
+	{
+		this.price = price;
+	}
+	
+	public void setStockType(String st)
+	{
+		this.stockType = st;
+	}
+	
+	public void setProductsID(int ID)
+	{
+		this.productsID = ID;
+	}
+	
+	public void setSuppliersID(int suppID)
+	{
+		this.suppliersID = suppID;
+	}
+	
+		
+}
