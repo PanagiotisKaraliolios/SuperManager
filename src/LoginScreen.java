b952c331a751d3b433eab5065895030cbd0971f1
Panagiotis_Karaliolios <karaliolios.panagiotis@gmail.com>
2020-05-02 04:17:48 +0300

    no message

--- src/LoginScreen.java
+++ src/LoginScreen.java
@@ -43,13 +43,13 @@ public class LoginScreen extends JFrame {
 		{
 			dialogPane.setBorder(new EmptyBorder(12, 12, 12, 12));
 			dialogPane.setBackground(new Color(0, 102, 204));
-			dialogPane.setBorder (new javax. swing. border. CompoundBorder( new javax .swing .border .TitledBorder (new javax.
-			swing. border. EmptyBorder( 0, 0, 0, 0) , "JFor\u006dDesi\u0067ner \u0045valu\u0061tion", javax. swing. border
-			. TitledBorder. CENTER, javax. swing. border. TitledBorder. BOTTOM, new java .awt .Font ("Dia\u006cog"
-			,java .awt .Font .BOLD ,12 ), java. awt. Color. red) ,dialogPane. getBorder
-			( )) ); dialogPane. addPropertyChangeListener (new java. beans. PropertyChangeListener( ){ @Override public void propertyChange (java
-			.beans .PropertyChangeEvent e) {if ("bord\u0065r" .equals (e .getPropertyName () )) throw new RuntimeException
-			( ); }} );
+			//dialogPane.setBorder (new javax. swing. border. CompoundBorder( new javax .swing .border .TitledBorder (new javax.
+			//swing. border. EmptyBorder( 0, 0, 0, 0) , "JFor\u006dDesi\u0067ner \u0045valu\u0061tion", javax. swing. border
+			//. TitledBorder. CENTER, javax. swing. border. TitledBorder. BOTTOM, new java .awt .Font ("Dia\u006cog"
+			//,java .awt .Font .BOLD ,12 ), java. awt. Color. red) ,dialogPane. getBorder
+			//( )) ); dialogPane. addPropertyChangeListener (new java. beans. PropertyChangeListener( ){ @Override public void propertyChange (java
+			//.beans .PropertyChangeEvent e) {if ("bord\u0065r" .equals (e .getPropertyName () )) throw new RuntimeException
+			//( ); }} );
 			dialogPane.setLayout(new BorderLayout());
 
 			//======== contentPanel ========
