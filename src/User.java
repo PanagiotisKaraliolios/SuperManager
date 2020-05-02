0a006762337a61a2208468c9121ce12be4079e6a
G_Krou <dai19061@uom.edu.gr>
2020-05-02 15:24:24 +0300

    Added class "User"

---
+++ src/User.java
@@ -0,0 +1,34 @@
+
+public class User {
+	private String username;
+	private String password;
+	
+	//Constructor
+	public User(String username, String password)
+	{
+		this.username = username;
+		this.password = password;
+	}
+
+	//get methods
+	public String getUsername() {
+		return username;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	//set methods
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+
+}
