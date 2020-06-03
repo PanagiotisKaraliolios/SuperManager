829b20ddd3161ee22169e5456f14f6cf10fce82a
TheofilosKiapidis <theofiloskiapidis@protonmail.com>
2020-06-03 11:32:58 +0300

    Added functionality to stats screen buttons, κάποιος που έχει sales να δοκιμάσει και να με πει άμα βγαίνει σωστά.

--- src/ViewStatisticsScreen.java
+++ src/ViewStatisticsScreen.java
@@ -1,5 +1,11 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyVetoException;
+import java.sql.*;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+
 import javax.swing.*;
 import javax.swing.table.*;
 import com.github.lgooddatepicker.components.*;
@@ -87,12 +93,31 @@ public class ViewStatisticsScreen extends JInternalFrame {
 				button1.setIcon(new ImageIcon(getClass().getResource("/checkMark.png")));
 				contentPane.add(button1);
 				button1.setBounds(new Rectangle(new Point(140, 200), button1.getPreferredSize()));
+				button1.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e)
+					{
+						try
+						{
+							confirmButtonAction(e);
+						}
+						catch (Exception e1)
+						{
+							e1.printStackTrace();
+						}
+					}
+				});
 
 				//---- button2 ----
 				button2.setText("Advanced Statistics");
 				button2.setIcon(new ImageIcon(getClass().getResource("/stats.png")));
 				contentPane.add(button2);
 				button2.setBounds(new Rectangle(new Point(75, 305), button2.getPreferredSize()));
+				button2.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e)
+					{
+						advancedStatisticsButtonAction(e);
+					}
+				});
 
 				//---- datePicker1 ----
 				DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
@@ -145,6 +170,7 @@ public class ViewStatisticsScreen extends JInternalFrame {
 			panel1.setPreferredSize(new Dimension(745, 465));
 		}
 		// JFormDesigner - End of component initialization  //GEN-END:initComponents
+		
 	}
 
 	// JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
@@ -152,10 +178,10 @@ public class ViewStatisticsScreen extends JInternalFrame {
 	private JPanel panel1;
 	private JScrollPane scrollPane1;
 	private JTable table1;
-	private JButton button1;
-	private JButton button2;
-	private DatePicker datePicker1;
-	private DatePicker datePicker2;
+	private JButton button1; //confirm button
+	private JButton button2; //advanced statistics button
+	private DatePicker datePicker1; //from Field
+	private DatePicker datePicker2; //to Field
 	private JLabel label2;
 	private JLabel label1;
 	private JLabel label3;
@@ -164,4 +190,61 @@ public class ViewStatisticsScreen extends JInternalFrame {
 	private DatePickerSettings datePickerSettings1;
 	private DatePickerSettings datePickerSettings2;
 	// JFormDesigner - End of variables declaration  //GEN-END:variables
+	
+	private void confirmButtonAction(ActionEvent e) throws SQLException, ClassNotFoundException
+	{
+		String from = datePicker1.getComponentDateTextField().getText();
+		String to = datePicker2.getComponentDateTextField().getText();
+		int inputID;
+		if(!textField1.getText().equals(""))
+		{
+			inputID = Integer.parseInt(textField1.getText());
+			if(from.equals("") || to .equals(""))
+				JOptionPane.showMessageDialog(null, "No date given", "ERROR", 2);
+			else
+			{
+				//Empty the table
+				DefaultTableModel model = (DefaultTableModel) table1.getModel();
+				for(int i = model.getRowCount()-1; i>=0; i--)
+					model.removeRow(i);
+				
+				//fill the table with new input
+				Class.forName("com.mysql.cj.jdbc.Driver");
+				Connection con = DriverManager.getConnection("jdbc:mysql://localhost/sm", "root", "");
+				Statement stm = con.createStatement();
+				
+				ResultSet rs = stm.executeQuery("SELECT products.name, sales.quantity "
+						+ "FROM products JOIN sales ON sales.id = products.id "
+						+ "WHERE sales.date >= '" + from + "' AND sales.date <= '" + to + "' AND sales.id = " + inputID);
+				
+				ArrayList<String> names = new ArrayList<>();
+				ArrayList<Integer> quantities = new ArrayList<>(); 
+				//get names and quantities of products
+				while(rs.next())
+				{
+					names.add(rs.getString("products.name"));
+					quantities.add(rs.getInt("sales.quantity"));
+				}
+				//fill talbe
+				for(int i = 0; i<names.size(); i++)
+					model.addRow(new Object[] {names.get(i), quantities.get(i)});
+			}
+		}
+		else
+			JOptionPane.showMessageDialog(null, "No ID given", "ERROR", 2);		
+	}
+	
+	private void advancedStatisticsButtonAction(ActionEvent e)
+	{
+		ViewAdvancedStatisticsScreen ASC = new ViewAdvancedStatisticsScreen();
+		
+		this.getParent().add(ASC);
+		try
+		{
+			this.setClosed(true);
+		} catch (PropertyVetoException e1)
+		{
+			e1.printStackTrace();
+		}
+	}
 }
